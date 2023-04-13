a94185c2e7aebccced0d30a83faa4a7b7a360460
pornoiya <49945883+pornoiya@users.noreply.github.com>
2023-04-13 21:43:14 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import anderson_ksamp
+from hyppo.ksample import MMD
 
 chat_id = 146155552 # Ваш chat ID, не меняйте название переменной
 
@@ -8,4 +8,5 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return anderson_ksamp([x, y]).pvalue < 0.06
+    stat, pvalue = MMD().test(x, y)
+    return pvalue < 0.06
